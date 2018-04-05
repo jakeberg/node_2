dfc7d829a69be58777d3d827605d9d98ce77076c
Jake Berg <jacobvictorberg@gmail.com>
2018-04-05 09:43:45 -0400

    Finished assignment

--- scores.js
+++ scores.js
@@ -17,7 +17,6 @@ const hostname = null;
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-
     if (req.method === "GET") {
         if (resources[req.url] === undefined) {
             res.statusCode = 404;
@@ -28,15 +27,25 @@ const server = http.createServer((req, res) => {
     } else if (req.method === "POST") {
         res.statusCode = 201;
         jsonBody(req, res, function (err, body) {
-            scores.push(body);
+                scores.push(body);
         })
+        if (resources[req.url] === undefined) {
+            res.statusCode = 404;
+        }
         if (scores.length == 3) {
-            scores.shift();
+            scores.pop();
         }
     }
 
+    scores.sort(compare);
+    function compare(a, b) {
+        if (a.score > b.score)
+            return -1;
+        if (a.score < b.score)
+            return 1;
+        return 0;
+    }
 
-    // console.log(keysorted)
     res.end(JSON.stringify(scores));
 });
 
